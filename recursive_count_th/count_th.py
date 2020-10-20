d4387f7e5960407305480ae916a806c67b9c5816
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-10-19 17:56:58 -0700

    removed comments

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,8 +6,6 @@ Your function must utilize recursion. It cannot contain any loops.
 word = ["abcdefghithkthkth"]
 def count_th(word):
     #need to sort through all letters in the word[0],
-    # then sort through all letters in the substring,
-    # then compare both sorted strings and return words where matches where found  
     # TBC
     #base case if length of word is less than 2 characters return 0
     if len(word) < 2:
@@ -16,6 +14,6 @@ def count_th(word):
     #if word range == "th" return the count 
     if word[:2] == "th":
         return count_th(word[1:]) + 1
-        
+
     #run function recursively 
     return count_th(word[1:])
